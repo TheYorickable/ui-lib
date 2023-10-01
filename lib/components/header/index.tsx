8c6a7bbe8ac534d8dd3198d4a75b085dee050ad9
Yorick Toma <yorick.toma@capgemini.com>
2023-10-02 00:33:18 +0200

    Need children in header

--- lib/components/header/index.tsx
+++ lib/components/header/index.tsx
@@ -4,25 +4,35 @@ import styles from "./styles.module.css";
 
 interface HeaderProps {
   navigate: NavigateFunction;
+  children?: React.ReactNode;
   title: string;
   time: string;
   className?: string;
 }
 
 export const Header: React.FC<HeaderProps> = (props: HeaderProps) => {
-  const { navigate, title, time, className = "", ...restProps } = props;
+  const {
+    navigate,
+    title,
+    time,
+    className = "",
+    children,
+    ...restProps
+  } = props;
 
   return (
-    <header className={`${styles.header} ${className}`} {...restProps}>
+    <header
+      title={title}
+      className={`${styles.header} ${className}`}
+      {...restProps}
+    >
       <div
         className={styles.title}
         onClick={() => {
           navigate("/");
         }}
       >
-        <h3>
-          Y<span>orick</span>T<span>oma</span>
-        </h3>
+        <h3>{children}</h3>
       </div>
       <div className={styles.date}>
         <span>{time}</span>
