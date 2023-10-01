5b5f4a97df337c0efa479c43bb679b9bc7e30ba9
Yorick Toma <yorick.toma@capgemini.com>
2023-10-01 22:09:03 +0200

    Basic setup and example page

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { PropsWithChildren, ReactNode } from "react";
+import { Button, Input, Label } from "../";
+import "./App.css";
+import styles from "./App.module.css";
+
+const Example = ({
+  title,
+  children,
+}: {
+  title?: string;
+  children: ReactNode;
+}) => (
+  <div className={styles.component}>
+    <h1>{title}</h1>
+    {children}
+  </div>
+);
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Example title={"Button"}>
+        <Button>Aap</Button>
+      </Example>
+      <Example title={"Label"}>
+        <Label>Aap</Label>
+      </Example>
+      <Example>
+        <Input defaultValue="Input" />
+      </Example>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
